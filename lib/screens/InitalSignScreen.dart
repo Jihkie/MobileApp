c20fc26a465cf2be469458fb460c73b756c1752a
Jihkie <jihkie@users.noreply.github.com>
2025-03-16 00:48:02 -0700

    Resize logo on get started screen to fit device

--- lib/screens/InitalSignScreen.dart
+++ lib/screens/InitalSignScreen.dart
@@ -36,57 +36,60 @@ class InitalSignScreenState extends State<InitalSignScreen> {
     final String assetName = 'images/logos/Icon.svg';
     final Widget svg = SvgPicture.asset(
       assetName,
+      fit: BoxFit.contain,
     );
 
     return Scaffold(
       body: Column(
         children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                width: context.width(),
-                height: context.height(),
-                color: Color(0x18141c),
-              ),
-              Column(
-                children: [
-                  svg,
-                  SizedBox(height: 25),
-                  Text('A truly Shocking experience.',
-                      style: boldTextStyle(color: white, size: 30),
-                      textAlign: TextAlign.center),
-                  16.height,
-                  Text(
-                      'OpenShock is an open-source platform designed to control various shocking devices over the internet, catering to all your masochistic needs! ',
-                      style: primaryTextStyle(color: white),
-                      textAlign: TextAlign.center),
-                  16.height,
-                  AppButton(
-                      textColor: white,
-                      color: Color(0xe14a6d),
+          Expanded(
+            child:Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: context.width(),
+                  height: context.height(),
+                  color: Color(0x18141c),
+                ),
+                Column(
+                  children: [
+                    Expanded(child:svg),
+                    SizedBox(height: 25),
+                    Text('A truly Shocking experience.',
+                        style: boldTextStyle(color: white, size: 30),
+                        textAlign: TextAlign.center),
+                    16.height,
+                    Text(
+                        'OpenShock is an open-source platform designed to control various shocking devices over the internet, catering to all your masochistic needs! ',
+                        style: primaryTextStyle(color: white),
+                        textAlign: TextAlign.center),
+                    16.height,
+                    AppButton(
+                        textColor: white,
+                        color: Color(0xe14a6d),
+                        width: context.width(),
+                        text: 'Get Started',
+                        onTap: () {
+                          launchUrl(Uri.parse(
+                              'https://openshock.app/#/account/signup'));
+                        }),
+                    16.height,
+                    AppButton(
+                      color: context.cardColor,
+                      text: 'Sign In',
+                      textStyle: boldTextStyle(),
                       width: context.width(),
-                      text: 'Get Started',
+                      shapeBorder: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
                       onTap: () {
-                        launchUrl(Uri.parse(
-                            'https://openshock.app/#/account/signup'));
-                      }),
-                  16.height,
-                  AppButton(
-                    color: context.cardColor,
-                    text: 'Sign In',
-                    textStyle: boldTextStyle(),
-                    width: context.width(),
-                    shapeBorder: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)),
-                    onTap: () {
-                      finish(context);
-                      SignInScreen().launch(context);
-                    },
-                  ),
-                ],
-              ).paddingSymmetric(vertical: 16, horizontal: 16)
-            ],
+                        finish(context);
+                        SignInScreen().launch(context);
+                      },
+                    ),
+                  ],
+                ).paddingSymmetric(vertical: 16, horizontal: 16)
+              ],
+            ),
           ),
         ],
       ),
